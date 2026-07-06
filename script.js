f686777f99490ddbec70fa26715bfb2562a9525f
arsenykarimov <arsenii00797@gmail.com>
2026-07-06 19:04:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,22 @@
 const menu = document.querySelector('.menu');
 const menuIcon = document.querySelector('.menu-icon');
 
-let opened = true;
-
-
-// first automatic reveal
+let opened = false;
 
 setTimeout(() => {
   menu.classList.add('menu-opened');
   menu.classList.remove('menu-closed');
+  opened = true;
 }, 7200);
 
-
-// manual control
-
 menuIcon.addEventListener('click', () => {
-
   if (opened) {
-
     menu.classList.remove('menu-opened');
     menu.classList.add('menu-closed');
-
     opened = false;
-
   } else {
-
     menu.classList.remove('menu-closed');
     menu.classList.add('menu-opened');
-
     opened = true;
-
   }
-
 });
