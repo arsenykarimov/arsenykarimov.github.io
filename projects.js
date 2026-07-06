d0d4976c74b522777b11c818445cfec7123b196d
arsenykarimov <arsenii00797@gmail.com>
2026-07-06 23:59:53 +0300

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,7 +1,14 @@
-{
-  title: "RETROGRADE",
-  subtitle: "SHORTFILM",
-  color: "#F6FFAD",
-  loop: "video/retrograde-loop.mp4",
-  trailer: "https://pub-e6c268da1b654b929cf0eb9763a0a6e1.r2.dev/Trailers/retrograde-trailer.mp4"
-}
+const projects = [
+
+  {
+    title: "RETROGRADE",
+    subtitle: "SHORTFILM",
+
+    titleFont: "#F6FFAD",
+    arrowsColor: "#F6FFAD",
+
+    loop: "video/retrograde-loop.mp4",
+    trailer: "https://pub-e6c268da1b654b929cf0eb9763a0a6e1.r2.dev/Trailers/retrograde-trailer.mp4"
+  }
+
+];
