b3ac84975decab74d867793ad30f93a72b1431b3
arsenykarimov <arsenii00797@gmail.com>
2026-07-06 18:20:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const menu = document.querySelector('.menu');
+const menuIcon = document.querySelector('.menu-icon');
 
+let menuOpened = true;
+
+menuIcon.addEventListener('click', () => {
+  menuOpened = !menuOpened;
+
+  if (menuOpened) {
+    menu.classList.remove('menu-closed');
+    menu.classList.add('menu-opened');
+  } else {
+    menu.classList.remove('menu-opened');
+    menu.classList.add('menu-closed');
+  }
+});
