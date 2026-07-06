58c96b22f1bf74571119e7bb11b3e98ced23e9a1
arsenykarimov <arsenii00797@gmail.com>
2026-07-07 00:01:38 +0300

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,13 +1,15 @@
 const projects = [
 
   {
+    id: "retrograde",
+
     title: "RETROGRADE",
     subtitle: "SHORTFILM",
 
-    titleFont: "#F6FFAD",
-    arrowsColor: "#F6FFAD",
+    accent: "#F6FFAD",
 
     loop: "video/retrograde-loop.mp4",
+
     trailer: "https://pub-e6c268da1b654b929cf0eb9763a0a6e1.r2.dev/Trailers/retrograde-trailer.mp4"
   }
 
