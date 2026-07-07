90be27fdb38b0261f18ed972c0d1485f330c9a06
arsenykarimov <arsenii00797@gmail.com>
2026-07-07 08:41:20 +0300

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,43 +1,28 @@
 const projects = [
-
   {
     id: "retrograde",
-
     title: "RETROGRADE",
     subtitle: "SHORTFILM",
-
     accent: "#F6FFAD",
-
     loop: "video/retrograde-loop.mp4",
-
     trailer: "https://pub-e6c268da1b654b929cf0eb9763a0a6e1.r2.dev/Trailers/retrograde-trailer.mp4"
   },
 
-
   {
     id: "paraneba",
-
     title: "PARANEBA",
     subtitle: "COMMERCIAL",
-
     accent: "#DBFFA8",
-
     loop: "video/paraneba-loop.mp4",
-
     trailer: ""
   },
 
-{
-  id: "anikushin",
-
-  title: "ANIKUSHIN\nMUSEUM",
-  subtitle: "EXHIBITION",
-
-  accent: "#A8E5FF",
-
-  loop: "video/anikushin-loop.mp4",
-
-  trailer: ""
-}
-
+  {
+    id: "anikushin",
+    title: "ANIKUSHIN\nMUSEUM",
+    subtitle: "EXHIBITION",
+    accent: "#A8E5FF",
+    loop: "video/anikushin-loop.mp4",
+    trailer: ""
+  }
 ];
