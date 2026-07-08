7dd73cac5c1b24b06264619fee519bfaa772d4d0
arsenykarimov <arsenii00797@gmail.com>
2026-07-08 22:10:20 +0300

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,1 +1,12 @@
+const menuButton = document.querySelector('.contact-menu-button');
+const menuOverlay = document.querySelector('.contact-menu-overlay');
 
+menuButton.addEventListener('click', () => {
+  document.body.classList.toggle('contact-menu-active');
+});
+
+menuOverlay.addEventListener('click', (event) => {
+  if (!event.target.closest('.contact-menu-links')) {
+    document.body.classList.remove('contact-menu-active');
+  }
+});
