29da1db84043010e01c09c4db0db525c3e471668
arsenykarimov <arsenii00797@gmail.com>
2026-07-17 02:55:25 +0300

    Update project.js

--- ru/project.js
+++ ru/project.js
@@ -1,1 +1,41 @@
-lol
+const projects = [
+  {
+    id: "retrograde",
+    title: "РЕТРОГРАД",
+    subtitle: "КОРОТКОМЕТРАЖНЫЙ ФИЛЬМ",
+    accent: "#F6FFAD",
+    poster: "../posters/retrograde.jpg",
+    loop: "../video/retrograde-loop.mp4",
+    trailer: "https://pub-e6c268da1b654b929cf0eb9763a0a6e1.r2.dev/Trailers/retrograde-trailer.mp4"
+  },
+
+  {
+    id: "paraneba",
+    title: "ПАРАНЕБА",
+    subtitle: "РЕКЛАМА",
+    accent: "#DBFFA8",
+    poster: "../posters/paraneba.jpg",
+    loop: "../video/paraneba-loop.mp4",
+    trailer: "https://pub-e6c268da1b654b929cf0eb9763a0a6e1.r2.dev/Trailers/paraneba-trailer.mp4"
+  },
+
+  {
+    id: "anikushin",
+    title: "МУЗЕЙ\nАНИКУШИНА",
+    subtitle: "ВЫСТАВКА",
+    accent: "#A8E5FF",
+    poster: "../posters/anikushin.jpg",
+    loop: "../video/anikushin-loop.mp4",
+    trailer: "https://pub-e6c268da1b654b929cf0eb9763a0a6e1.r2.dev/Trailers/anikushin-trailer.mp4"
+  },
+
+  {
+    id: "anikushin2",
+    title: "МУЗЕЙ\nАНИКУШИНА",
+    subtitle: "ВЫСТАВКА",
+    accent: "#A8E5FF",
+    poster: "../posters/anikushin_2.jpg",
+    loop: "../video/anikushin_2-loop.mp4",
+    trailer: "https://pub-e6c268da1b654b929cf0eb9763a0a6e1.r2.dev/Trailers/anikushin_2.mp4"
+  },
+];
